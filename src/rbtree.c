38da688d69ad830664c76f2b188b6b80968b4a4d
dsmrd <dsmrd@users.noreply.github.com>
2016-09-16 11:59:17 +0200

    Applied coding standard

--- src/rbtree.c
+++ src/rbtree.c
@@ -1,3 +1,28 @@
+/*
+ * DSMRd - Dutch Smart Meter Requirements Daemon
+ * Copyright (C)2016  M.J. de Wit
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+/*
+ * Heavily modified source code based on following implementation:
+ * <https://www.cs.auckland.ac.nz/~jmor159/PLDS210/niemann/s_rbt.txt>
+ * No original copyrights found.
+ */
+
 /* red-black tree */
 
 #include <stdio.h>
@@ -6,39 +31,41 @@
 #include <stdarg.h>
 
 
-typedef int T;                  /* type of item to be stored */
+typedef int T;                  // type of item to be stored
 #define compLT(a,b) (a < b)
 #define compEQ(a,b) (a == b)
 
-/* Red-Black tree description */
-typedef enum { BLACK, RED } nodeColor;
+typedef struct rbnode_struct_t* rbnode_t;
 
-typedef struct Node_ {
-	struct Node_ *left;         /* left child */
-	struct Node_ *right;        /* right child */
-	struct Node_ *parent;       /* parent */
-	nodeColor color;            /* node color (BLACK, RED) */
-	T data;                     /* data stored in node */
-} Node;
+// Red-Black tree description
+typedef enum { BLACK, RED } rbnode_color_t;
 
-#define NIL &sentinel           /* all leafs are sentinels */
-Node sentinel = { NIL, NIL, 0, BLACK, 0};
+struct rbnode_struct_t {
+	rbnode_t left;          // left child
+	rbnode_t right;         // right child
+	rbnode_t parent;        // parent
+	rbnode_color_t color;   // node color (BLACK, RED)
+	T data;                 // data stored in node
+};
 
-Node *root = NIL;               /* root of Red-Black tree */
+#define NIL &sentinel           // all leafs are sentinels
+struct rbnode_struct_t sentinel = { NIL, NIL, 0, BLACK, 0};
 
-void rotateLeft(Node *x) {
+rbnode_t root = NIL;               // root of Red-Black tree
 
-	/**************************
-	 *  rotate node x to left *
-	 **************************/
+void rbnode_rotate_left(rbnode_t x) {
 
-	Node *y = x->right;
+	//
+	// rotate node x to left
+	//
 
-	/* establish x->right link */
+	rbnode_t y = x->right;
+
+	// establish x->right link
 	x->right = y->left;
 	if (y->left != NIL) y->left->parent = x;
 
-	/* establish y->parent link */
+	// establish y->parent link
 	if (y != NIL) y->parent = x->parent;
 	if (x->parent) {
 		if (x == x->parent->left)
@@ -49,24 +76,24 @@ void rotateLeft(Node *x) {
 		root = y;
 	}
 
-	/* link x and y */
+	// link x and y
 	y->left = x;
 	if (x != NIL) x->parent = y;
 }
 
-void rotateRight(Node *x) {
+void rbnode_rotate_right(rbnode_t x) {
 
-	/****************************
-	 *  rotate node x to right  *
-	 ****************************/
+	//
+	// rotate node x to right
+	//
 
-	Node *y = x->left;
+	rbnode_t y = x->left;
 
-	/* establish x->left link */
+	// establish x->left link
 	x->left = y->right;
 	if (y->right != NIL) y->right->parent = x;
 
-	/* establish y->parent link */
+	// establish y->parent link
 	if (y != NIL) y->parent = x->parent;
 	if (x->parent) {
 		if (x == x->parent->right)
@@ -77,79 +104,79 @@ void rotateRight(Node *x) {
 		root = y;
 	}
 
-	/* link x and y */
+	// link x and y
 	y->right = x;
 	if (x != NIL) x->parent = y;
 }
 
-void insertFixup(Node *x) {
+void rbnode_insert_fixup(rbnode_t x) {
 
-	/*************************************
-	 *  maintain Red-Black tree balance  *
-	 *  after inserting node x           *
-	 *************************************/
+	//
+	// maintain Red-Black tree balance
+	// after inserting node x
+	//
 
-	/* check Red-Black properties */
+	// check Red-Black properties
 	while (x != root && x->parent->color == RED) {
-		/* we have a violation */
+		// we have a violation
 		if (x->parent == x->parent->parent->left) {
-			Node *y = x->parent->parent->right;
+			rbnode_t y = x->parent->parent->right;
 			if (y->color == RED) {
 
-				/* uncle is RED */
+				// uncle is RED
 				x->parent->color = BLACK;
 				y->color = BLACK;
 				x->parent->parent->color = RED;
 				x = x->parent->parent;
 			} else {
 
-				/* uncle is BLACK */
+				// uncle is BLACK
 				if (x == x->parent->right) {
-					/* make x a left child */
+					// make x a left child
 					x = x->parent;
-					rotateLeft(x);
+					rbnode_rotate_left(x);
 				}
 
-				/* recolor and rotate */
+				// recolor and rotate
 				x->parent->color = BLACK;
 				x->parent->parent->color = RED;
-				rotateRight(x->parent->parent);
+				rbnode_rotate_right(x->parent->parent);
 			}
 		} else {
 
-			/* mirror image of above code */
-			Node *y = x->parent->parent->left;
+			// mirror image of above code
+			rbnode_t y = x->parent->parent->left;
 			if (y->color == RED) {
 
-				/* uncle is RED */
+				// uncle is RED
 				x->parent->color = BLACK;
 				y->color = BLACK;
 				x->parent->parent->color = RED;
 				x = x->parent->parent;
 			} else {
 
-				/* uncle is BLACK */
+				// uncle is BLACK
 				if (x == x->parent->left) {
 					x = x->parent;
-					rotateRight(x);
+					rbnode_rotate_right(x);
 				}
 				x->parent->color = BLACK;
 				x->parent->parent->color = RED;
-				rotateLeft(x->parent->parent);
+				rbnode_rotate_left(x->parent->parent);
 			}
 		}
 	}
 	root->color = BLACK;
 }
 
-Node *insertNode(T data) {
-	Node *current, *parent, *x;
+rbnode_t rbnode_insert(T data) {
+	rbnode_t current, parent, x;
 
-	/***********************************************
-	 *  allocate node for data and insert in tree  *
-	 ***********************************************/
+	//
+	// allocate node for data and insert in tree
+	//
 
-	/* find where node belongs */
+	// find where node belongs
 	current = root;
 	parent = 0;
 	while (current != NIL) {
@@ -159,9 +186,9 @@ Node *insertNode(T data) {
 			current->left : current->right;
 	}
 
-	/* setup new node */
+	// setup new node
 	if ((x = malloc (sizeof(*x))) == 0) {
-		printf ("insufficient memory (insertNode)\n");
+		printf ("insufficient memory (rbnode_insert)\n");
 		exit(1);
 	}
 	x->data = data;
@@ -170,7 +197,7 @@ Node *insertNode(T data) {
 	x->right = NIL;
 	x->color = RED;
 
-	/* insert node in tree */
+	// insert node in tree
 	if(parent) {
 		if(compLT(data, parent->data))
 			parent->left = x;
@@ -180,24 +207,24 @@ Node *insertNode(T data) {
 		root = x;
 	}
 
-	insertFixup(x);
+	rbnode_insert_fixup(x);
 	return(x);
 }
 
-void deleteFixup(Node *x) {
+void rbnode_delete_fixup(rbnode_t x) {
 
-	/*************************************
-	 *  maintain Red-Black tree balance  *
-	 *  after deleting node x            *
-	 *************************************/
+	//
+	// maintain Red-Black tree balance
+	// after deleting node x
+	//
 
 	while (x != root && x->color == BLACK) {
 		if (x == x->parent->left) {
-			Node *w = x->parent->right;
+			rbnode_t w = x->parent->right;
 			if (w->color == RED) {
 				w->color = BLACK;
 				x->parent->color = RED;
-				rotateLeft (x->parent);
+				rbnode_rotate_left (x->parent);
 				w = x->parent->right;
 			}
 			if (w->left->color == BLACK && w->right->color == BLACK) {
@@ -207,21 +234,21 @@ void deleteFixup(Node *x) {
 				if (w->right->color == BLACK) {
 					w->left->color = BLACK;
 					w->color = RED;
-					rotateRight (w);
+					rbnode_rotate_right (w);
 					w = x->parent->right;
 				}
 				w->color = x->parent->color;
 				x->parent->color = BLACK;
 				w->right->color = BLACK;
-				rotateLeft (x->parent);
+				rbnode_rotate_left (x->parent);
 				x = root;
 			}
 		} else {
-			Node *w = x->parent->left;
+			rbnode_t w = x->parent->left;
 			if (w->color == RED) {
 				w->color = BLACK;
 				x->parent->color = RED;
-				rotateRight (x->parent);
+				rbnode_rotate_right (x->parent);
 				w = x->parent->left;
 			}
 			if (w->right->color == BLACK && w->left->color == BLACK) {
@@ -231,13 +258,13 @@ void deleteFixup(Node *x) {
 				if (w->left->color == BLACK) {
 					w->right->color = BLACK;
 					w->color = RED;
-					rotateLeft (w);
+					rbnode_rotate_left (w);
 					w = x->parent->left;
 				}
 				w->color = x->parent->color;
 				x->parent->color = BLACK;
 				w->left->color = BLACK;
-				rotateRight (x->parent);
+				rbnode_rotate_right (x->parent);
 				x = root;
 			}
 		}
@@ -245,32 +272,32 @@ void deleteFixup(Node *x) {
 	x->color = BLACK;
 }
 
-void deleteNode(Node *z) {
-	Node *x, *y;
+void rbnode_delete(rbnode_t z) {
+	rbnode_t x, y;
 
-	/*****************************
-	 *  delete node z from tree  *
-	 *****************************/
+	//
+	// delete node z from tree
+	//
 
 	if (!z || z == NIL) return;
 
 
 	if (z->left == NIL || z->right == NIL) {
-		/* y has a NIL node as a child */
+		// y has a NIL node as a child
 		y = z;
 	} else {
-		/* find tree successor with a NIL node as a child */
+		// find tree successor with a NIL node as a child
 		y = z->right;
 		while (y->left != NIL) y = y->left;
 	}
 
-	/* x is y's only child */
+	// x is y's only child
 	if (y->left != NIL)
 		x = y->left;
 	else
 		x = y->right;
 
-	/* remove y from the parent chain */
+	// remove y from the parent chain
 	x->parent = y->parent;
 	if (y->parent)
 		if (y == y->parent->left)
@@ -284,18 +311,18 @@ void deleteNode(Node *z) {
 
 
 	if (y->color == BLACK)
-		deleteFixup (x);
+		rbnode_delete_fixup (x);
 
 	free (y);
 }
 
-Node *findNode(T data) {
+rbnode_t rbnode_find(T data) {
 
-	/*******************************
-	 *  find node containing data  *
-	 *******************************/
+	//
+	// find node containing data
+	//
 
-	Node *current = root;
+	rbnode_t current = root;
 	while(current != NIL)
 		if(compEQ(data, current->data))
 			return (current);
@@ -307,25 +334,25 @@ Node *findNode(T data) {
 
 void main(int argc, char **argv) {
 	int a, maxnum, ct;
-	Node *t;
+	rbnode_t t;
 
-	/* command-line:
-	 *
-	 *   rbt maxnum
-	 *
-	 *   rbt 2000
-	 *       process 2000 records
-	 *
-	 */
+	// command-line:
+	//
+	//   rbt maxnum
+	//
+	//   rbt 2000
+	//       process 2000 records
+	//
+	//
 
 	maxnum = atoi(argv[1]);
 
 	for (ct = maxnum; ct; ct--) {
 		a = rand() % 9 + 1;
-		if ((t = findNode(a)) != NULL) {
-			deleteNode(t);
+		if ((t = rbnode_find(a)) != NULL) {
+			rbnode_delete(t);
 		} else {
-			insertNode(a);
+			rbnode_insert(a);
 		}
 	}
 }

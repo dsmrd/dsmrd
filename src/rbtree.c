772f42e187848265a595b6b5c06f44ff31a27b98
dsmrd <dsmrd@users.noreply.github.com>
2016-09-16 23:16:44 +0200

    Added tree functions

--- src/rbtree.c
+++ src/rbtree.c
@@ -31,10 +31,10 @@
 #include <stdarg.h>
 
 
-typedef int (*rbnode_less_then)(void* a, void* b);
-typedef int (*rbnode_equals)(void* a, void* b);
-typedef void (*rbnode_free_key)(void* a);
-typedef void (*rbnode_free_value)(void* a);
+typedef int (*rbtree_less_then_t)(void* a, void* b);
+typedef int (*rbtree_equals_t)(void* a, void* b);
+typedef void (*rbtree_free_key_t)(void* a);
+typedef void (*rbtree_free_value_t)(void* a);
 
 typedef struct rbnode_struct_t* rbnode_t;
 typedef struct rbtree_struct_t* rbtree_t;
@@ -47,23 +47,23 @@ struct rbnode_struct_t {
 	rbnode_t right;         // right child
 	rbnode_t parent;        // parent
 	rbnode_color_t color;   // node color (BLACK, RED)
-	void* key;                 // key stored in node
-	void* value;                 // value stored in node
+	void* key;              // key stored in node
+	void* value;            // value stored in node
 };
 
 #define NIL &sentinel           // all leafs are sentinels
-struct rbnode_struct_t sentinel = { NIL, NIL, 0, BLACK, 0};
+struct rbnode_struct_t sentinel = { NIL, NIL, NULL, BLACK, NULL, NULL};
 
 struct rbtree_struct_t {
 	rbnode_t root;               // root of Red-Black tree
-	rbnode_less_then less_than;
-	rbnode_equals equals;
-	rbnode_free_key free_key;
-	rbnode_free_value free_value;
+	rbtree_less_then_t less_then;
+	rbtree_equals_t equals;
+	rbtree_free_key_t free_key;
+	rbtree_free_value_t free_value;
 };
 
 
-static void rbnode_rotate_left(rbnode_t x, rbtree_t t) {
+static void _rbtree_rotate_left(rbtree_t t, rbnode_t x) {
 
 	//
 	// rotate node x to left
@@ -96,7 +96,7 @@ static void rbnode_rotate_left(rbnode_t x, rbtree_t t) {
 	}
 }
 
-static void rbnode_rotate_right(rbnode_t x, rbtree_t t) {
+static void _rbtree_rotate_right(rbtree_t t, rbnode_t x) {
 
 	//
 	// rotate node x to right
@@ -129,7 +129,7 @@ static void rbnode_rotate_right(rbnode_t x, rbtree_t t) {
 	}
 }
 
-static void rbnode_insert_fixup(rbnode_t x, rbtree_t t) {
+static void _rbtree_put_fixup(rbtree_t t, rbnode_t x) {
 
 	//
 	// maintain Red-Black tree balance
@@ -154,13 +154,13 @@ static void rbnode_insert_fixup(rbnode_t x, rbtree_t t) {
 				if (x == x->parent->right) {
 					// make x a left child
 					x = x->parent;
-					rbnode_rotate_left(x, t);
+					_rbtree_rotate_left(t, x);
 				}
 
 				// recolor and rotate
 				x->parent->color = BLACK;
 				x->parent->parent->color = RED;
-				rbnode_rotate_right(x->parent->parent, t);
+				_rbtree_rotate_right(t, x->parent->parent);
 			}
 		} else {
 
@@ -178,19 +178,20 @@ static void rbnode_insert_fixup(rbnode_t x, rbtree_t t) {
 				// uncle is BLACK
 				if (x == x->parent->left) {
 					x = x->parent;
-					rbnode_rotate_right(x, t);
+					_rbtree_rotate_right(t, x);
 				}
 				x->parent->color = BLACK;
 				x->parent->parent->color = RED;
-				rbnode_rotate_left(x->parent->parent, t);
+				_rbtree_rotate_left(t, x->parent->parent);
 			}
 		}
 	}
 	t->root->color = BLACK;
 }
 
-rbnode_t rbnode_insert(void* key, void* value, rbtree_t t) {
+void* rbtree_put(rbtree_t t, void* key, void* value) {
 	rbnode_t current, parent, x;
+	void* rval = 0;
 
 	//
 	// allocate node for key and insert in tree
@@ -201,16 +202,18 @@ rbnode_t rbnode_insert(void* key, void* value, rbtree_t t) {
 	parent = 0;
 	while (current != NIL) {
 		if (t->equals(key, current->key)) {
-			return (current);
+			rval = current->value;
+			current->value = value;
+			return rval;
 		}
 		parent = current;
-		current = t->less_than(key, current->key) ?
+		current = t->less_then(key, current->key) ?
 			current->left : current->right;
 	}
 
 	// setup new node
 	if ((x = malloc (sizeof(*x))) == 0) {
-		printf ("insufficient memory (rbnode_insert)\n");
+		printf ("insufficient memory (rbtree_put)\n");
 		exit(1);
 	}
 	x->key = key;
@@ -222,7 +225,7 @@ rbnode_t rbnode_insert(void* key, void* value, rbtree_t t) {
 
 	// insert node in tree
 	if(parent) {
-		if(t->less_than(key, parent->key)) {
+		if(t->less_then(key, parent->key)) {
 			parent->left = x;
 		} else {
 			parent->right = x;
@@ -231,11 +234,12 @@ rbnode_t rbnode_insert(void* key, void* value, rbtree_t t) {
 		t->root = x;
 	}
 
-	rbnode_insert_fixup(x, t);
-	return(x);
+	_rbtree_put_fixup(t, x);
+
+	return NULL;
 }
 
-static void rbnode_delete_fixup(rbnode_t x, rbtree_t t) {
+static void _rbtree_delete_node_fixup(rbtree_t t, rbnode_t x) {
 
 	//
 	// maintain Red-Black tree balance
@@ -248,7 +252,7 @@ static void rbnode_delete_fixup(rbnode_t x, rbtree_t t) {
 			if (w->color == RED) {
 				w->color = BLACK;
 				x->parent->color = RED;
-				rbnode_rotate_left(x->parent, t);
+				_rbtree_rotate_left(t, x->parent);
 				w = x->parent->right;
 			}
 			if (w->left->color == BLACK && w->right->color == BLACK) {
@@ -258,13 +262,13 @@ static void rbnode_delete_fixup(rbnode_t x, rbtree_t t) {
 				if (w->right->color == BLACK) {
 					w->left->color = BLACK;
 					w->color = RED;
-					rbnode_rotate_right(w, t);
+					_rbtree_rotate_right(t, w);
 					w = x->parent->right;
 				}
 				w->color = x->parent->color;
 				x->parent->color = BLACK;
 				w->right->color = BLACK;
-				rbnode_rotate_left(x->parent, t);
+				_rbtree_rotate_left(t, x->parent);
 				x = t->root;
 			}
 		} else {
@@ -272,7 +276,7 @@ static void rbnode_delete_fixup(rbnode_t x, rbtree_t t) {
 			if (w->color == RED) {
 				w->color = BLACK;
 				x->parent->color = RED;
-				rbnode_rotate_right(x->parent, t);
+				_rbtree_rotate_right(t, x->parent);
 				w = x->parent->left;
 			}
 			if (w->right->color == BLACK && w->left->color == BLACK) {
@@ -282,13 +286,13 @@ static void rbnode_delete_fixup(rbnode_t x, rbtree_t t) {
 				if (w->left->color == BLACK) {
 					w->right->color = BLACK;
 					w->color = RED;
-					rbnode_rotate_left(w, t);
+					_rbtree_rotate_left(t, w);
 					w = x->parent->left;
 				}
 				w->color = x->parent->color;
 				x->parent->color = BLACK;
 				w->left->color = BLACK;
-				rbnode_rotate_right(x->parent, t);
+				_rbtree_rotate_right(t, x->parent);
 				x = t->root;
 			}
 		}
@@ -296,8 +300,9 @@ static void rbnode_delete_fixup(rbnode_t x, rbtree_t t) {
 	x->color = BLACK;
 }
 
-void rbnode_delete(rbnode_t z, rbtree_t t) {
+static void* _rbtree_delete_node(rbtree_t t, rbnode_t z) {
 	rbnode_t x, y;
+	void* rval;
 
 	//
 	// delete node z from tree
@@ -339,22 +344,24 @@ void rbnode_delete(rbnode_t z, rbtree_t t) {
 
 	if (y != z) {
 		t->free_key(z->key);
-		t->free_value(z->value);
+		rval = z->value;
 		z->key = y->key;
 		z->value = y->value;
 	} else {
 		t->free_key(y->key);
-		t->free_value(y->value);
+		rval = y->value;
 	}
 
 	if (y->color == BLACK) {
-		rbnode_delete_fixup(x, t);
+		_rbtree_delete_node_fixup(t, x);
 	}
 
 	free (y);
+
+	return rval;
 }
 
-rbnode_t rbnode_find(void* key, rbtree_t t) {
+static rbnode_t _rbtree_find_node(void* key, rbtree_t t) {
 
 	//
 	// find node containing key
@@ -363,14 +370,55 @@ rbnode_t rbnode_find(void* key, rbtree_t t) {
 	rbnode_t current = t->root;
 	while(current != NIL) {
 		if(t->equals(key, current->key)) {
-			return (current);
+			return current;
 		} else {
-			current = t->less_than(key, current->key) ?
+			current = t->less_then(key, current->key) ?
 				current->left : current->right;
 		}
 	}
 
-	return(0);
+	return NULL;
+}
+
+void* rbtree_get(rbtree_t inst, void* key) {
+	void* rval = NULL;
+	rbnode_t node;
+
+	node = _rbtree_find_node(key, inst);
+	if (node != NULL) {
+		rval = node->value;
+	}
+
+	return rval;
+}
+
+void* rbtree_delete(rbtree_t inst, void* key) {
+	void* rval = NULL;
+	rbnode_t node;
+
+	node = _rbtree_find_node(key, inst);
+	if (node != NULL) {
+		rval = _rbtree_delete_node(inst, node);
+	}
+
+	return rval;
+}
+
+rbtree_t rbtree_init(rbtree_less_then_t less_then, rbtree_equals_t equals, rbtree_free_key_t free_key, rbtree_free_value_t free_value) {
+	rbtree_t inst;
+
+	inst = (rbtree_t) malloc(sizeof(struct rbtree_struct_t));
+	inst->root = NIL;
+	inst->less_then = less_then;
+	inst->equals = equals;
+	inst->free_key = free_key;
+	inst->free_value = free_value;
+
+	return inst;
+}
+
+void rbtree_exit(rbtree_t inst) {
+	free(inst);
 }
 
 int compLT(void* a, void* b) {
@@ -391,14 +439,16 @@ void FV(void* k) {
 
 void main(int argc, char **argv) {
 	int a, maxnum, ct;
-	rbnode_t t;
-	struct rbtree_struct_t r;
+	rbnode_t n;
+	rbtree_t t;
+	void* v;
 
-	r.root = NIL;
-	r.less_than = compLT;
-	r.equals = compEQ;
-	r.free_key = FK;
-	r.free_value = FV;
+	t = rbtree_init(compLT, compEQ, FK, FV);
+	//r.root = NIL;
+	//r.less_then = compLT;
+	//r.equals = compEQ;
+	//r.free_key = FK;
+	//r.free_value = FV;
 
 	// command-line:
 	//
@@ -435,19 +485,23 @@ void main(int argc, char **argv) {
 
 	for (ct = maxnum; ct; ct--) {
 		a = rand() % 9 + 1;
-		if ((t = rbnode_find(s[a].key, &r)) != NULL) {
-			printf("Deleting %s value %s\n", (char*)t->key, (char*)t->value);
-			rbnode_delete(t, &r);
+		if ((n = _rbtree_find_node(s[a].key, t)) != NULL) {
+			printf("Deleting %s value %s\n", (char*)n->key, (char*)n->value);
+			v = _rbtree_delete_node(t, n);
+			FV(v);
 		} else {
-			t = rbnode_insert(strdup(s[a].key), s[a].key, &r);
-			printf("Inserting %s value %s\n", (char*)t->key, (char*)t->value);
+			v = rbtree_put(t, strdup(s[a].key), s[a].key);
+			printf("Inserting %s\n", s[a].key);
+			printf("Value %p returned\n", v);
 		}
 	}
 
 	for (a = 0; a < 9; a++) {
-		if ((t = rbnode_find(s[a].key, &r)) != NULL) {
-			printf("Deleting %s value %s\n", (char*)t->key, (char*)t->value);
-			rbnode_delete(t, &r);
+		if ((rbtree_get(t, s[a].key)) != NULL) {
+			printf("Deleting %s *)\n", s[a].key);
+			//v = _rbtree_delete_node(t, n);
+			v = rbtree_delete(t, s[a].key);
+			FV(v);
 		}
 	}
 }

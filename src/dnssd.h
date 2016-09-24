3ba2f3936ac92ad2b4800fcca6d066d106e68132
dsmrd <dsmrd@users.noreply.github.com>
2016-09-24 16:42:57 +0200

    Added copyrights

--- src/dnssd.h
+++ src/dnssd.h
@@ -1,3 +1,24 @@
+/*
+ * DSMRd - Dutch Smart Meter Requirements Daemon
+ * Copyright (C)2016  M.J. de Wit
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef DNSSD_H
+#define DNSSD_H
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -15,4 +36,6 @@ typedef struct ava_struct_t* avahi_t;
 avahi_t avahi_init(char* name);
 void avahi_open(avahi_t inst, dispatch_t dis);
 void ava_close(avahi_t inst);
+
+#endif // DNSSD_H
 
--- src/list.h
+++ src/list.h
@@ -1,3 +1,25 @@
+/*
+ * DSMRd - Dutch Smart Meter Requirements Daemon
+ * Copyright (C)2016  M.J. de Wit
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef LIST_H
+#define LIST_H
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -33,4 +55,6 @@ bool iter_next(iter_t inst);
 bool iter_prev(iter_t inst);
 /*@null@*/ node_t iter_get_node(iter_t inst);
 /*@null@*/ void* iter_get(iter_t inst);
+
+#endif // LIST_H
 
